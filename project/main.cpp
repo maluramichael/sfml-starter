076541e88fbd2f5fd365090a1bea9181c4340bbf
Michael Malura <michael.malura@socialbit.de>
2016-03-28 21:01:19 +0200

    Add framwork lib
    Move assets to bin directory

--- project/main.cpp
+++ project/main.cpp
@@ -1,22 +1,30 @@
-#include <SFML/Window.hpp>
-#include <SFML/Graphics/RenderWindow.hpp>
 #include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Hello sfml\n";
 
-    sf::RenderWindow window(sf::VideoMode(480, 480), "Window");
-    sf::Event event;
-    while (window.isOpen()) {
-        while(window.pollEvent(event)){
-            if (event.type == sf::Event::Closed){
-                window.close();
-            }
-        }
-        window.clear(sf::Color::Black);
-        window.display();
-    }
+	sf::Shader shader;
+
+	if (!shader.loadFromFile("noise.vert", "noise.frag")) {
+		return 1;
+	}
+
+	sf::RectangleShape r(sf::Vector2f(2, 1));
+
+	sf::RenderWindow window(sf::VideoMode(480, 480), "Window");
+	sf::Event event;
+
+	while (window.isOpen()) {
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+		window.clear(sf::Color::Black);
+		window.display();
+	}
 
 
-    return 0;
+	return 0;
 }
